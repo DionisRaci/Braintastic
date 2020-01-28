82eec8925e271cac193fdee883143d5dae16b448
DionisRaci <dionis_4@hotmail.com>
2020-01-28 11:29:55 +0100

    finished User entity

--- braintasticendpoint/src/main/java/at/braintastic/braintasticendpoint/entity/User.java
+++ braintasticendpoint/src/main/java/at/braintastic/braintasticendpoint/entity/User.java
@@ -1,10 +1,14 @@
 package at.braintastic.braintasticendpoint.entity;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@NamedQueries(
+        @NamedQuery(
+                name = "User.findAll",
+                query = "select u from User u"
+        )
+)
+@Table(name = "BT_User", schema = "braintasticdb")
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.TABLE)
     private long id;
