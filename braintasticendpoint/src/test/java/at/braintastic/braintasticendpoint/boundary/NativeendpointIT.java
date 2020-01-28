abfa4049d63978f9293dd2851f3895e655220861
DionisRaci <dionis_4@hotmail.com>
2020-01-28 10:23:54 +0100

    initialized project

---
+++ braintasticendpoint/src/test/java/at/braintastic/braintasticendpoint/boundary/NativeendpointIT.java
@@ -0,0 +1,9 @@
+package at.braintastic.braintasticendpoint.boundary;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeendpointIT extends endpointTest {
+
+    // Execute the same tests but in native mode.
+}
