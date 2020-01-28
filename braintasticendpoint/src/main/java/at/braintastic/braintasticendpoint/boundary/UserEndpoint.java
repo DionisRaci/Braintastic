64a7df9dc4b2c5269f4f490fe6c4a4f2fc218cc2
DionisRaci <dionis_4@hotmail.com>
2020-01-28 11:30:20 +0100

    Created GET request (not tested)

--- braintasticendpoint/src/main/java/at/braintastic/braintasticendpoint/boundary/UserEndpoint.java
+++ braintasticendpoint/src/main/java/at/braintastic/braintasticendpoint/boundary/UserEndpoint.java
@@ -1,16 +1,23 @@
 package at.braintastic.braintasticendpoint.boundary;
 
+import at.braintastic.braintasticendpoint.control.UserRepository;
+import at.braintastic.braintasticendpoint.entity.User;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.List;
 
-@Path("/hello")
+@Path("/user")
 public class UserEndpoint {
+    @Inject
+    UserRepository userRepository;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
--- braintasticendpoint/src/main/java/at/braintastic/braintasticendpoint/control/UserRepository.java
+++ braintasticendpoint/src/main/java/at/braintastic/braintasticendpoint/control/UserRepository.java
@@ -1,13 +1,20 @@
 package at.braintastic.braintasticendpoint.control;
 
+import at.braintastic.braintasticendpoint.entity.User;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
+import java.util.List;
 
 @ApplicationScoped
 @Transactional
 public class UserRepository {
     @PersistenceContext
     EntityManager em;
+
+    public List<User> findAll() {
+        return em.createNamedQuery("User.findAll").getResultList();
+    }
 }
