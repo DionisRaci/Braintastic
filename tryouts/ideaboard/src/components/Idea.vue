2e51ed63a7b80d380aabf6055880a40bb7ac79a7
Gabriel Aigner <gabsi.aigner@gmail.com>
2020-02-28 22:34:46 +0100

    Add Idea component

---
+++ tryouts/ideaboard/src/components/Idea.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="card shadow1">
+      <div class="content">
+        <p></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
