bcf60c2c164646fd3311ea0d4178354d3190bd7b
Gabriel Aigner <gabsi.aigner@gmail.com>
2020-02-28 22:35:03 +0100

    Update Idea component

--- tryouts/ideaboard/src/components/Idea.vue
+++ tryouts/ideaboard/src/components/Idea.vue
@@ -13,4 +13,30 @@ export default {};
 </script>
 
 <style>
+p {
+  font-size: 14px;
+}
+.content {
+}
+
+textarea {
+  padding: 5px;
+  border: 1px solid white;
+  resize: none;
+}
+textarea:hover {
+  padding: 5px;
+  border: 1px solid white;
+  resize: both;
+}
+textarea:after {
+  padding: 5px;
+  border: 1px solid black;
+  resize: both;
+}
+
+.card {
+  background: #fff;
+  display: inline-block;
+}
 </style>
