0120b1001080eb8a29a4f9330181c5a7ba6a1683
Gabriel Aigner <gabsi.aigner@gmail.com>
2020-02-29 10:16:59 +0100

    Add IdeaInput component

---
+++ tryouts/ideaboard/src/components/IdeaInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <input
+      type="text"
+      id="ideaInput"
+      v-on:keyup.enter="onEnter"
+      v-model="text"
+      placeholder="Write your superduper creative idea in here..."
+    />
+    <button id="ideaSubmit" @click="onSubmitIdea">Sumbit Idea</button>
+    <p>{{count}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "",
+      count: 0
+    };
+  },
+  props: {
+    ideaText: String
+  },
+  methods: {
+    onSubmitIdea() {
+      this.ideaText = this.text;
+      console.log(this.ideaText);
+      this.count += 1;
+      this.clearInput();
+    },
+    onEnter() {
+      document.getElementById("ideaSubmit").click();
+    },
+    clearInput(event) {
+      document.getElementById("ideaInput").blur();
+      this.text = "";
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
