a0785cb2bf719bc2d684df6b80a6fb57b086872c
David <david.cvetkovic01@gmail.com>
2020-03-02 09:20:37 +0100

    create Webpages project

---
+++ tryouts/Webpages/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+  <app-join></app-join>
+ </div>
+</template>
+
+<script>
+import Login from "./components/login.vue";
+import Join from "./components/join.vue";
+import Create from "./components/create.vue";
+
+export default {
+ components:{
+   "app-login" : Login,
+   "app-join" : Join,
+   "app-create" : Create
+ }
+}
+</script>
+
+<style>
+
+</style>
---
+++ tryouts/Webpages/src/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+ <div>
+ <h1>Servus David, wir sind in Lgin Vue</h1>
+ </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style scoped>
+h1{
+    color:red;
+}
+</style>
