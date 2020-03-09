15e2292219a9b78c376d637fe0537151ec76a2d0
Gabriel Aigner <gabsi.aigner@gmail.com>
2020-03-09 07:19:18 +0100

    Add eventBus

--- tryouts/ideaboard/src/main.js
+++ tryouts/ideaboard/src/main.js
@@ -1,7 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+export const eventBus = new Vue({
+  methods: {
+    addIdea(ideaText) {
+      this.$emit("addedIdea", ideaText);
+    }
+  }
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
