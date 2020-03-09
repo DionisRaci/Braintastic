1739dc7404e3f5b1224a42fe2c528392d158bec8
Gabriel Aigner <gabsi.aigner@gmail.com>
2020-03-09 07:20:15 +0100

    Update Idea : New Styling

--- tryouts/ideaboard/src/components/Idea.vue
+++ tryouts/ideaboard/src/components/Idea.vue
@@ -1,42 +1,22 @@
 <template>
-  <div>
-    <div class="card shadow1">
-      <div class="content">
-        <p></p>
-      </div>
-    </div>
+  <div class="idea">
+    <p>{{ideaText}}</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    ideaText: String
+  }
+};
 </script>
 
-<style>
-p {
-  font-size: 14px;
-}
-.content {
-}
-
-textarea {
-  padding: 5px;
-  border: 1px solid white;
-  resize: none;
-}
-textarea:hover {
-  padding: 5px;
-  border: 1px solid white;
-  resize: both;
-}
-textarea:after {
-  padding: 5px;
-  border: 1px solid black;
-  resize: both;
-}
-
-.card {
-  background: #fff;
+<style scoped>
+.idea {
+  background-color: white;
+  margin: 20px;
+  padding: 20px;
   display: inline-block;
 }
 </style>
