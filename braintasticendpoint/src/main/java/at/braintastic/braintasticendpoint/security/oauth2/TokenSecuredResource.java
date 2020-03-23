186712d95ae51f55b8f424d2cc9d7bd8ff0564a7
sandro6880 <sandro6880@gmail.com>
2020-03-23 09:51:40 +0100

    Sandro: Added simple Token Endpoint

---
+++ braintasticendpoint/src/main/java/at/braintastic/braintasticendpoint/security/oauth2/TokenSecuredResource.java
@@ -0,0 +1,58 @@
+package at.braintastic.braintasticendpoint.security.oauth2;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
+import javax.annotation.security.PermitAll;
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
+import java.security.Principal;
+
+@Path("/secured")
+@RequestScoped
+public class TokenSecuredResource {
+
+    @Inject
+    JsonWebToken jwt;
+
+    @GET()
+    @Path("permit-all")
+    @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(@Context SecurityContext ctx) {
+        Principal caller =  ctx.getUserPrincipal();
+        String name = caller == null ? "anonymous" : caller.getName();
+        boolean hasJWT = jwt.getClaimNames() != null;
+        String helloReply =
+                String.format("hello + %s, isSecure: %s, authScheme: %s, hasJWT: %s",
+                        name,
+                        ctx.isSecure(),
+                        ctx.getAuthenticationScheme(),
+                        hasJWT);
+        return helloReply;
+    }
+
+    @GET()
+    @Path("roles-allowed")
+    @RolesAllowed({"Admin", "Member"})
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloRolesAllowed(@Context SecurityContext ctx) {
+        Principal caller =  ctx.getUserPrincipal();
+        String name = caller == null ? "anonymous" : caller.getName();
+        boolean hasJWT = jwt.getClaimNames() != null;
+        String helloReply =
+                String.format("hello + %s, isSecure: %s, authScheme: %s, hasJWT: %s",
+                        name,
+                        ctx.isSecure(),
+                        ctx.getAuthenticationScheme(),
+                        hasJWT);
+        return helloReply;
+    }
+
+}
