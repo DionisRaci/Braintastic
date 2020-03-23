444cdba2f7af610311da0bf6c305a14aa3beb3a0
Gabriel Aigner <gabsi.aigner@gmail.com>
2020-03-23 08:39:52 +0100

    Add Webproject

---
+++ braintasticfrontend/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <login></login>
+    <register></register>
+  </div>
+</template>
+
+<script>
+import WebsocketGreetings from "./components/WebsocketGreetings.vue";
+import Login from "./components/Login.vue";
+import Register from "./components/Register.vue";
+
+export default {
+  components: {
+    WebsocketGreetings,
+    Login,
+    Register
+  }
+};
+</script>
+
+<style>
+/* Removes Browser scrollbar in ...*/
+html {
+  -ms-overflow-style: none; /* Internet Explorer 10+ */
+  scrollbar-width: none; /* Firefox */
+}
+::-webkit-scrollbar {
+  display: none; /* Safari and Chrome */
+}
+</style>
