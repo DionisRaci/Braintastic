cc708e1ad4f6c4fc41771c08cfd78f696221a837
gabrielaigner <gabsi.aigner@gmail.com>
2020-05-17 17:18:52 +0200

    Update Card: Removed some stuff

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -1,23 +1,29 @@
 <template>
   <div class="mt-8 bg-white overflow-hidden shadow rounded-lg">
-    <div class="border-b border-gray-200 px-4 py-5 sm:px-6">
+    <div class=" px-4 py-5 sm:px-6">
       <p>{{ text }}</p>
       <!-- Content goes here -->
       <!-- We use less vertical padding on card headers on desktop than on body sections -->
-    </div>
-    <div class="border-t-1 border-gray-200 px-4 py-4 sm:px-6">
-      <!-- Content goes here -->
-      <!-- We use less vertical padding on card footers at all sizes than on headers or body sections -->
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
+const baseURL = 'http://localhost:8080/'
+
 export default {
   props: [
     'text',
-    ''
-  ]
+    'id'
+  ],
+  created () {
+    axios.get(baseURL + 'Session/' + this.sessionId + '/ideas')
+      .then(res => {
+        this.ideas = res.data
+      })
+  }
 
 }
 </script>
