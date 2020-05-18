2c77f44e94eeeb62f0af1821b6f8e21680104805
gabrielaigner <gabsi.aigner@gmail.com>
2020-05-18 11:58:37 +0200

    Delete unneedet tags

--- frontend/src/components/Card.vue
+++ frontend/src/components/Card.vue
@@ -2,8 +2,6 @@
   <div class="mt-8 bg-white overflow-hidden shadow rounded-lg">
     <div class=" px-4 py-5 sm:px-6">
       <p>{{ text }}</p>
-      <!-- Content goes here -->
-      <!-- We use less vertical padding on card headers on desktop than on body sections -->
     </div>
   </div>
 </template>
@@ -11,12 +9,7 @@
 <script>
 export default {
   props: [
-    'text',
-    'id'
+    'text'
   ]
 }
 </script>
-
-<style>
-
-</style>
